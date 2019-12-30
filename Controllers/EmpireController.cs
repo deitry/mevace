acd21a36570bbdf60dcc1b77557c6490a6ba6469
Dmitriy Vornychev <dm.s.vornychev@gmail.com>
2019-12-30 13:02:49 +0300

    Добавлен новый контроллер с параметризуемым запросом

---
+++ Controllers/EmpireController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Encodings.Web;
+
+namespace mevace.Controllers
+{
+    public class EmpireController : Controller
+    {
+
+        private readonly ILogger<EmpireController> _logger;
+
+        public EmpireController(ILogger<EmpireController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string name = "Ru", int id = 0)
+        {
+            // Структура запроса: обрати внимание, что id может обрабатываться специально
+            // http://localhost:5000/Empire/Index/1?name=Chelovek
+
+            ViewData["Message"] = $"Hello, this is {name} President speaking";
+            ViewData["NumTimes"] = id;
+
+            return View("Index1");
+            // return HtmlEncoder.Default.Encode($"Hello, this is {name} President speaking");
+        }
+
+
+    }
+}
